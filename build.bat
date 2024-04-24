1d13140c98a422126325a8cf73f2f93a1634f974
Danny Gitelman <55748709+gitelmandanny@users.noreply.github.com>
2024-04-24 20:58:07 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,10 @@
 echo "Building Package $ENV:PACKAGE_NAME"
-  
+dasdasdasdhasda'dasdh
+daskdjasdasd
+
+djkdaskdsadas
+ddajdkasdjaskdjaskdas
+
 if ( $ENV:RUN_TEST -eq "true" )
     {
 		echo "Running tests"
